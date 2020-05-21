f37323c7cb3de11b90c2968367b6600c160f4cef
zappaboy <federico_zappone@hotmail.it>
2020-05-21 17:17:12 +0200

    Added Crawler script and updated README

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 # MusicGenRetor
 Music genre classification with Machine Learning
+
+First of all you need to start musicWgetter.sh, this script download testing music sorted my genres.
+
+Supported genres are:
+- Blues 
+- Electronic 
+- Classical 
+- Pop 
+- Rock 
+- Jazz
+
+Install musicWgetter.sh dependencies:
+
+```
+# Debian based
+sudo apt-get install parallel
+
+# Arch Linux based
+sudo pacman -S parallel
+```
+
+Start musicWgetter:
+
+```
+sudo chmod +x musicWgetter.sh
+
+sh musicWgetter.sh
+```
---
+++ musicWgetter.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+BASE_URL='https://freemusicarchive.org/genre/'
+GENRES='Blues Electronic Classical Pop Rock Jazz'
+
+mkdir -p "songs"
+cd songs
+
+for GENRE in $GENRES; do
+
+    echo "Crawling: ${GENRE} "
+    CRAWLING_URL=${BASE_URL}${GENRE}
+
+    echo "URL: ${CRAWLING_URL}"
+    mkdir -p $GENRE
+    cd $GENRE
+
+    for PAGE_NUMBER in 12; do
+        PAGE_PARAMS="?sort=track_date_published&d=1&page=${PAGE_NUMBER}"
+        PAGE_URL=${CRAWLING_URL}${PAGE_PARAMS}
+
+        wget $PAGE_URL -q -O ${GENRE}_${PAGE_NUMBER}
+
+        grep -Po '(?<=href=")[^"]*' ${GENRE}_${PAGE_NUMBER} | grep '/music' | parallel --gnu "wget -A '.mp3' {} "
+        echo -e "\n"
+    done
+
+    cd ..
+done
