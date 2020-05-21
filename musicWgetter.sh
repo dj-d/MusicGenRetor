b521948432a9b7634adbee1084d47bb3d2767eca
zappaboy <federico_zappone@hotmail.it>
2020-05-21 18:18:21 +0200

    Replaced for expansion with normal for statement due to systems compatibility

--- musicWgetter.sh
+++ musicWgetter.sh
@@ -7,15 +7,13 @@ mkdir -p "songs"
 cd songs
 
 for GENRE in $GENRES; do
-
     echo "Crawling: ${GENRE} "
     CRAWLING_URL=${BASE_URL}${GENRE}
-
     echo "URL: ${CRAWLING_URL}"
-    mkdir -p $GENRE
-    cd $GENRE
 
-    for PAGE_NUMBER in {1..12} ; do
+    mkdir -p $GENRE && cd $GENRE
+
+    for (( PAGE_NUMBER=1 ; i <= 12; i++ )) ; do
         PAGE_PARAMS="?sort=track_date_published&d=1&page=${PAGE_NUMBER}"
         PAGE_URL=${CRAWLING_URL}${PAGE_PARAMS}
 
