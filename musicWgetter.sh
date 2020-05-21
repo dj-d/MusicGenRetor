44fad0fbe7e5187253e400a90f1a06b8d1026191
zappaboy <federico_zappone@hotmail.it>
2020-05-21 22:37:46 +0200

    Added dynamic regex

--- musicWgetter.sh
+++ musicWgetter.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-BASE_URL='https://freemusicarchive.org/genre/'
+BASE_URL='https://freemusicarchive.org/'
+GENRE_ENDPOINT='genre/'
+MUSIC_ENDPOINT='music/'
+
 GENRES='Blues Electronic Classical Pop Rock Jazz'
 
 mkdir -p "songs"
@@ -8,20 +11,25 @@ cd songs
 
 for GENRE in $GENRES; do
     echo "Crawling: ${GENRE} "
-    CRAWLING_URL=${BASE_URL}${GENRE}
+    CRAWLING_URL=${BASE_URL}${GENRE_ENDPOINT}${GENRE}
     echo "URL: ${CRAWLING_URL}"
 
     mkdir -p $GENRE && cd $GENRE
 
-    for PAGE_NUMBER in {1..12} ; do
+    for PAGE_NUMBER in {1..1} ; do
         PAGE_PARAMS="?sort=track_date_published&d=1&page=${PAGE_NUMBER}"
         PAGE_URL=${CRAWLING_URL}${PAGE_PARAMS}
 
         wget $PAGE_URL -q -O ${GENRE}_${PAGE_NUMBER}
 
-        grep -Po '(?<=href=")[^"]*' ${GENRE}_${PAGE_NUMBER} | grep '/music' | parallel --gnu "wget -A '.mp3' {} "
+	BASE_URL_ESCAPED=$(${BASE_URL@Q}${MUSIC_ENDPOINT@Q} | tr -d \')
+	URL_REGEX='(?<=href=")'${BASE_URL_ESCAPED}'[^"]*'
+
+        grep -Po ${URL_REGEX} ${GENRE}_${PAGE_NUMBER} | parallel --gnu "wget -A '.mp3' {} "
+
         echo -e "\n"
     done
 
+    rm ${GENRE}_* *.tmp*
     cd ..
 done
