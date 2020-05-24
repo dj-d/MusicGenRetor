4ac79a27cf334fae03ffe38488f9a5ed81899cc6
Daniele Albanese <daniele.albanese98@gmail.com>
2020-05-25 00:41:57 +0200

    dataset_creator.py -> Converted from script to class

--- src/dataset_creator.py
+++ src/dataset_creator.py
@@ -1,74 +1,83 @@
-"""
-This script create the dataset needed to ML Algorithms
-"""
-
-import os
-import sys
+from os import path, walk, listdir
+from sys import stdout
 
 import pandas as pd
 from pip._vendor.distlib.compat import raw_input
 
 from src.audio_features import AudioFeatures
 
-script_path = os.path.dirname(os.path.realpath(__file__))
-dir_path = os.path.abspath(script_path + '/..') + '/songs'
-
-actual_id = 0
-attrs = ['Title', 'bpm', 'zero_crossing_rate', 'audio_time_series']
-df = pd.DataFrame(columns=attrs)
-
-recreate = True
-dataset_name = 'Dataset'
+DEFAULT_FOLDER_NAME = 'songs'
+DEFAULT_INDEX = ['Title', 'bpm', 'zero_crossing_rate', 'audio_time_series']
+DEFAULT_DATASET_NAME = 'dataset'
 
 
-def populate(data_path):
-    global actual_id
-    print('Loading data from:\t' + data_path)
-    for root, directories, files in os.walk(data_path):
-        for directory in directories:
-            subdir_path = os.path.join(data_path, directory)
-            os.walk(subdir_path)
-            print('---------- ' + directory + ' ----------')
-            files = os.listdir(subdir_path)
-            for file in files:
-                print(file)
-                file_path = os.path.join(subdir_path, file)
-                load_song(file_path)
+class DatasetCreator:
+    def __init__(self, folder_name=DEFAULT_FOLDER_NAME, index=None, dataset_name=DEFAULT_DATASET_NAME):
+        if index is None:
+            index = DEFAULT_INDEX
 
+        script_path = path.dirname(path.realpath(__file__))
+        self.dir_path = path.abspath(script_path + '/..') + '/' + folder_name
 
-def load_song(path):
-    global df, actual_id, attrs
-    audio_features = AudioFeatures(path)
+        self.index = index
+        self.data_frame = pd.DataFrame(columns=index)
 
-    # Following attrs list
-    title = os.path.basename(path)
-    bpm = audio_features.get_bpm()
-    zero_crossing_rate = audio_features.get_zero_crossing_rate()
-    audio_time_series = audio_features.get_audio_time_series()
+        self.dataset_name = dataset_name
+        self.id = 0
 
-    # Adding song to Dataset
-    song = pd.Series([title, bpm, zero_crossing_rate, audio_time_series], index=attrs)
-    df = df.append(song, ignore_index=True)
+    def load_song(self, path):
+        audio_features = AudioFeatures(path)
 
-    actual_id = actual_id + 1
+        title = path.basename(path)
+        bpm = audio_features.get_bpm()
+        zero_crossing_rate = audio_features.get_zero_crossing_rate()
+        audio_time_series = audio_features.get_audio_time_series()
 
+        song = pd.Series([title, bpm, zero_crossing_rate, audio_time_series], index=self.index)
+        self.data_frame = self.data_frame.append(song, ignore_index=True)
 
-# Starting Dataset Creation
-if os.path.exists(dataset_name):
-    print('Existing dataset')
-    recreate = False
-    sys.stdout.write('Create new dataset? ' + '[y/N]')
-    choice = raw_input().lower()
-    if choice == 'Y' or choice == 'y':
+        self.id += 1
+
+    def populate(self, data_path):
+        print('Loading data from: ' + data_path)
+
+        for root, directories, files in walk(data_path):
+            for directory in directories:
+                subdir_path = path.join(data_path, directory)
+
+                walk(subdir_path)
+
+                print('\n---------- ' + directory + ' ----------\n')
+
+                files = listdir(subdir_path)
+
+                for file in files:
+                    print(file)
+
+                    file_path = path.join(subdir_path, file)
+                    self.load_song(file_path)
+
+    def main(self):
         recreate = True
 
-    df = pd.read_pickle(dataset_name)
+        if path.exists(self.dataset_name):
+            print('Existing dataset')
 
-if recreate:
-    # Populating Dataset
-    populate(dir_path)
-    # Save Dataset
-    df.to_pickle(dataset_name)
-    print('Total records:\t' + str(actual_id))
+            recreate = False
 
-print(df)
+            stdout.write('Create new dataset? [y/N]')
+            choice = raw_input().lower()
+
+            if choice == 'Y' or choice == 'y':
+                recreate = True
+
+            self.data_frame = pd.read_pickle(self.dataset_name)
+
+        if recreate:
+            self.populate(self.dir_path)
+
+            self.data_frame.to_pickle(self.dataset_name)
+
+            print('Total records: ' + str(self.id))
+
+            print(self.data_frame)
