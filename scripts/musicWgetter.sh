9cd497f56b46fc5fa9cf1f8f40c62cf27c2716df
zappaboy <federico_zappone@hotmail.it>
2020-05-27 04:59:20 +0200

    musicWgetter: - Removed useless files and duplicated - suppressed "rm: not fount files" error - Optimized
    logic:
    - Added Clustering

--- scripts/musicWgetter.sh
+++ scripts/musicWgetter.sh
@@ -2,43 +2,47 @@
 
 BASE_URL='https://freemusicarchive.org/'
 GENRE_ENDPOINT='genre/'
-MUSIC_ENDPOINT='music/'
-
+PATTERN='https://files.freemusicarchive.org/storage-freemusicarchive-org/music/'
+BASE_URL_ESCAPED=$(echo ${PATTERN} | sed 's/\//\\\//g')
 GENRES='Blues Electronic Classical Pop Rock Jazz'
-
 DIR="$( cd "$( dirname "$0" )" && pwd )"
+PAGE_NUMBERS=2
 
+# Create songs directory
 cd "${DIR}/.."
 mkdir -p "songs" && cd songs
 
-for GENRE in $GENRES; do
+for GENRE in $GENRES ; do
     echo "Crawling: ${GENRE} "
     CRAWLING_URL=${BASE_URL}${GENRE_ENDPOINT}${GENRE}
     echo "URL: ${CRAWLING_URL}"
 
     mkdir -p $GENRE && cd $GENRE
 
-    for PAGE_NUMBER in {1..250} ; do
+    BEHIND_PATTERN=$(cat <<EOF
+>${GENRE}</a>[[:space:]]</span>[[:space:]]</div>[[:space:]]<span[[:space:]]class="playicn">[[:space:]]<a[[:space:]]href="
+EOF
+		  )
+    BEHIND_PATTERN_ESCAPED=$(echo ${BEHIND_PATTERN} | sed 's/\//\\\//g')
+    URL_REGEX='(?<='${BEHIND_PATTERN_ESCAPED}')'${BASE_URL_ESCAPED}'[^"]*'
+
+    for PAGE_NUMBER in $( seq 1 $PAGE_NUMBERS) ; do
         PAGE_PARAMS="?sort=track_date_published&d=1&page=${PAGE_NUMBER}"
         PAGE_URL=${CRAWLING_URL}${PAGE_PARAMS}
 
+	echo ${PAGE_URL}
+
         wget $PAGE_URL -q -O ${GENRE}_${PAGE_NUMBER}
-
-	PATTERN='https://files.freemusicarchive.org/storage-freemusicarchive-org/music/'
-	BASE_URL_ESCAPED=$(echo ${PATTERN} | sed 's/\//\\\//g')
-
-	BEHIND_PATTERN=$(cat <<EOF
->${GENRE}</a>[[:space:]]</span>[[:space:]]</div>[[:space:]]<span[[:space:]]class="playicn">[[:space:]]<a[[:space:]]href="
-EOF
-)
-	BEHIND_PATTERN_ESCAPED=$(echo ${BEHIND_PATTERN} | sed 's/\//\\\//g')
-	URL_REGEX='(?<='${BEHIND_PATTERN_ESCAPED}')'${BASE_URL_ESCAPED}'[^"]*'
-
 	grep -zPo ${URL_REGEX} ${GENRE}_${PAGE_NUMBER} | tr "\0" "\n" | xargs -0 -n1 | parallel --gnu "wget -A '.mp3' {}"
 
         echo -e "\n"
     done
 
-    rm ${GENRE}_* *.tmp*
+    # Remove HTML pages and tmp trash file
+    rm -f ${GENRE}_* *.tmp*
+
+    # Remove duplicated
+    rm -f *.1
+
     cd ..
 done
---
+++ src/logic.py
@@ -0,0 +1,29 @@
+import pandas as pd
+from sklearn.cluster import KMeans
+
+# TODO: change the script name to a more meaningful one
+
+# TODO: create a config file with attrs, dataset_name and genres
+attrs = ['Title', 'bpm', 'zero_crossing_rate', 'audio_time_series', 'genre']
+dataset_name = 'Dataset'
+genres = ['Blues' 'Electronic' 'Classical' 'Pop' 'Rock' 'Jazz']
+
+df = pd.DataFrame(columns=attrs)
+
+
+def load_dataset():
+    global df
+    return pd.read_pickle(dataset_name)
+
+
+def clustering():
+    dataset = load_dataset()
+    kmeans = KMeans(n_clusters=len(genres), random_state=0).fit(dataset)
+    # kmeans.labels_
+    print(kmeans.labels_)
+    kmeans.predict()
+    # kmeans.cluster_centers_
+    print(kmeans.cluster_centers_)
+
+
+clustering()
