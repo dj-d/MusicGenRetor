e824eef3404a13887fd699bac0933b0c6a7acba3
Daniele Albanese <daniele.albanese98@gmail.com>
2020-05-28 01:51:11 +0200

    audio_features.py -> Added parameters to plot graphics with outside series and added function to get spectral bandwidth + TODO -> add DOC and finish adding functions

--- src/audio_features.py
+++ src/audio_features.py
@@ -1,11 +1,10 @@
 import matplotlib.pyplot as plt
 
-from librosa import load, stft, amplitude_to_db, zero_crossings, frames_to_time
 from librosa.beat import tempo
-from librosa.display import waveplot, specshow
-from librosa.feature import spectral_centroid, spectral_rolloff, mfcc, chroma_stft
 from librosa.onset import onset_strength
-from librosa.util import example_audio_file
+from librosa.display import waveplot, specshow
+from librosa import load, stft, amplitude_to_db, zero_crossings, frames_to_time
+from librosa.feature import spectral_centroid, spectral_bandwidth, spectral_rolloff, mfcc, chroma_stft
 
 from sklearn.preprocessing import minmax_scale, scale
 
@@ -17,7 +16,7 @@ class AudioFeatures:
     Provide methods for audio visualization and audio info
     """
 
-    def __init__(self, audio_path=example_audio_file()):
+    def __init__(self, audio_path=None):
         """
         Class constructor
 
@@ -26,7 +25,32 @@ class AudioFeatures:
 
         warnings.filterwarnings('ignore')
 
-        self.y, self.sr = load(path=audio_path)
+        if audio_path is not None:
+            self.y, self.sr = load(path=audio_path)
+
+    def select_series(self, y):
+        """
+
+        :param y:
+        :return:
+        """
+
+        if y is not None:
+            return y
+        else:
+            return self.y
+
+    def select_sr(self, sr):
+        """
+
+        :param sr:
+        :return:
+        """
+
+        if sr is not None:
+            return sr
+        else:
+            return self.sr
 
     def get_audio_time_series(self):
         """
@@ -37,38 +61,54 @@ class AudioFeatures:
 
         return self.y
 
-    def plot_audio(self):
+    def plot_audio(self, outside_series=None, outside_sr=None):
         """
         Plot audio
+
+        :param outside_series:
+        :param outside_sr:
+        :return:
         """
 
+        y = self.select_series(outside_series)
+        sr = self.select_sr(outside_sr)
+
         plt.figure(figsize=(14, 5))
-        waveplot(self.y, sr=self.sr)
+        waveplot(y, sr=sr)
         plt.show()
 
-    # TODO: To improve
-    def plot_zoomed_audio(self, i, j):
+    def plot_zoomed_audio(self, i, j, outside_series=None):
         """
         Plot audio of in a range [i, j]
 
+        :param outside_series:
         :param i: start point
         :param j: end point
         """
 
+        y = self.select_series(outside_series)
+
         plt.figure(figsize=(14, 5))
-        plt.plot(self.y[i:j])
+        plt.plot(y[i:j])
         plt.grid()
         plt.show()
 
-    def plot_spectrogram(self):
+    def plot_spectrogram(self, outside_series=None, outside_sr=None):
         """
         Plot spectrogram of frequencies of audio
+
+        :param outside_series:
+        :param outside_sr:
+        :return:
         """
 
-        x_db = amplitude_to_db(abs(stft(self.y)))
+        y = self.select_series(outside_series)
+        sr = self.select_sr(outside_sr)
+
+        x_db = amplitude_to_db(abs(stft(y)))
 
         plt.figure(figsize=(14, 5))
-        specshow(x_db, sr=self.sr, x_axis='time', y_axis='hz')
+        specshow(x_db, sr=sr, x_axis='time', y_axis='hz')
         plt.show()
 
     def get_zero_crossing_rate(self):
@@ -91,6 +131,7 @@ class AudioFeatures:
         :param j: end point
         :return: Integer
         """
+
         zero_crossing = zero_crossings(self.y[i:j], pad=False)
 
         return sum(zero_crossing)
@@ -108,67 +149,120 @@ class AudioFeatures:
 
         return minmax_scale(x, axis=axis)
 
-    def plot_spectral_centroid(self):
-        """
-        Plot weighted average of the frequencies present in the sound
+    def get_spectral_centroid(self):
         """
 
-        spectral_centroids = spectral_centroid(self.y, sr=self.sr)[0]
+        :return:
+        """
+
+        return spectral_centroid(self.y, sr=self.sr)[0]
+
+    def plot_spectral_centroid(self, outside_series=None, outside_sr=None):
+        """
+        Plot weighted average of the frequencies present in the sound
+
+        :param outside_series:
+        :param outside_sr:
+        :return:
+        """
+
+        y = self.select_series(outside_series)
+        sr = self.select_sr(outside_sr)
+
+        spectral_centroids = spectral_centroid(y, sr=sr)[0]
 
         frames = range(len(spectral_centroids))
         t = frames_to_time(frames)
 
-        waveplot(self.y, sr=self.sr)
+        waveplot(y, sr=sr)
         plt.plot(t, self.normalize(spectral_centroids), color='r')
         plt.show()
 
+    def get_spectral_bandwidth(self):
+        """
+
+        :param outside_series:
+        :param outside_sr:
+        :return:
+        """
+
+        spectral_bandwidths = spectral_bandwidth(self.y, sr=self.sr)
+
+        return spectral_bandwidths
+
     # TODO: Add DOC
-    def plot_spectral_rolloff(self):
+    def plot_spectral_rolloff(self, outside_series=None, outside_sr=None):
         """
 
+        :param outside_series:
+        :param outside_sr:
+        :return:
         """
 
-        spectral_rolloffs = spectral_rolloff(self.y + 0.01, sr=self.sr)[0]
+        y = self.select_series(outside_series)
+        sr = self.select_sr(outside_sr)
+
+        spectral_rolloffs = spectral_rolloff(y + 0.01, sr=sr)[0]
 
         frames = range(len(spectral_rolloffs))
         t = frames_to_time(frames)
 
-        waveplot(self.y, sr=self.sr)
+        waveplot(y, sr=sr)
         plt.plot(t, self.normalize(spectral_rolloffs), color='r')
         plt.show()
 
-    def plot_mfcc(self):
+    def plot_mfcc(self, outside_series=None, outside_sr=None):
         """
         MFCC - Mel Frequency Cepstral Coefficients
 
         Plot small set of features that concisely describe the overall shape of a spectral envelope (curve tangent to a family of curves)
+
+        :param outside_series:
+        :param outside_sr:
+        :return:
         """
 
-        mfccs = mfcc(self.y, sr=self.sr)
+        y = self.select_series(outside_series)
+        sr = self.select_sr(outside_sr)
 
-        specshow(mfccs, sr=self.sr, x_axis='time')
+        mfccs = mfcc(y, sr=sr)
+
+        specshow(mfccs, sr=sr, x_axis='time')
         plt.show()
 
     # TODO: Add DOC
-    def plot_perform_mfcc(self):
+    def plot_perform_mfcc(self, outside_series=None, outside_sr=None):
         """
 
+        :param outside_series:
+        :param outside_sr:
+        :return:
         """
 
-        mfccs = mfcc(self.y, sr=self.sr)
+        y = self.select_series(outside_series)
+        sr = self.select_sr(outside_sr)
+
+        mfccs = mfcc(y, sr=sr)
         mfccs = scale(mfccs, axis=1)
 
-        specshow(mfccs, sr=self.sr, x_axis='time')
+        specshow(mfccs, sr=sr, x_axis='time')
         plt.show()
 
-    def plot_chroma_frequencies(self):
+    def plot_chroma_frequencies(self, outside_series=None, outside_sr=None):
         """
         Plot audio where the entire spectrum is projected on 12 bins representing the 12 semitones of the musical octave
+
+        :param outside_series:
+        :param outside_sr:
+        :return:
         """
+
+        y = self.select_series(outside_series)
+        sr = self.select_sr(outside_sr)
 
         hop_length = 512
 
-        chroma_gram = chroma_stft(self.y, sr=self.sr, hop_length=hop_length)
+        chroma_gram = chroma_stft(y, sr=sr, hop_length=hop_length)
 
         plt.figure(figsize=(14, 5))
         specshow(chroma_gram, x_axis='time', y_axis='chroma', hop_length=hop_length, cmap='coolwarm')
