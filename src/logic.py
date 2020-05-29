49a990816368d89e51721f6b4239843c7709ea70
zappaboy <federico_zappone@hotmail.it>
2020-05-29 04:35:58 +0200

    Added image model generation and read

--- src/logic.py
+++ src/logic.py
@@ -1,93 +1,77 @@
-import math
+import os
 
-import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
-from numpy import array
-from sklearn import preprocessing
-from sklearn.cluster import KMeans
-from sklearn.feature_extraction import image
+
+from src.audio_features import AudioFeatures
 
 # TODO: change the script name to a more meaningful one
 
 # TODO: create a config file with attrs, dataset_name and genres
 attrs = ['Title', 'bpm', 'zero_crossing_rate', 'audio_time_series', 'genre']
 dataset_name = 'Dataset'
-genres = ['Blues' 'Electronic' 'Classical' 'Pop' 'Rock' 'Jazz']
-
+genres = ['Blues', 'Electronic', 'Classical', 'Pop', 'Rock', 'Jazz']
+genres_dfs = {}
 df = pd.DataFrame(columns=attrs)
+rows = 20
+columns = 49
+sr = 22100
 
 
-def load_dataset():
+def load_dataset(n):
     global df
     # TODO: create bulk load function
-    return pd.read_pickle(dataset_name + '_1')
+    return pd.read_pickle(dataset_name + '_' + str(n))
 
 
-def clustering():
-    df = load_dataset()
-
-    ids = df[attrs[0]]
-    genres_true = df[attrs[len(attrs) - 1]]
-    dataset = df.loc[0:, attrs[1]:attrs[len(attrs) - 2]]
-    # dataset.loc[1:, attrs[1]:attrs[len(attrs) - 3]] = preprocessing.normalize(dataset.loc[1:, attrs[1]:attrs[len(attrs) - 3]])
-    # dataset = preprocessing.normalize(dataset)
-
-    kmeans = KMeans(n_clusters=len(genres), random_state=0).fit(dataset)
-    # kmeans.labels_
-    print(kmeans.labels_)
-    kmeans.predict()
-    # kmeans.cluster_centers_
-    print(kmeans.cluster_centers_)
+def load_image_model(model_name):
+    model = pd.read_pickle(model_name)
+    AudioFeatures().plot_perform_mfcc_by_values(model, sr)
 
 
-def spectral_analisys(img):
-    mask = img.astype(bool)
-    img = img.astype(float)
-    # Convert the image into a graph with the value of the gradient on the
-    # edges.
-    graph = image.img_to_graph(img, mask=mask)
-
-    # Take a decreasing function of the gradient: we take it weakly
-    # dependent from the gradient the segmentation is close to a voronoi
-    graph.data = np.exp(-graph.data / graph.data.std())
-
-    # Force the solver to be arpack, since amg is numerically
-    # unstable on this example
-    # labels = spectral_clustering(graph, n_clusters=4, eigen_solver='arpack')
-    # label_im = np.full(mask.shape, -1.)
-    # label_im[mask] = labels
-
-    plt.matshow(img)
-    # plt.matshow(label_im)
-    plt.show()
+def init():
+    # Init dictionary
+    for genre in genres:
+        genres_dfs[genre] = {
+            'model': pd.DataFrame(np.zeros((rows, columns))),
+            'n_songs': 0
+        }
 
 
-def array_to_img(arr):
-    # x = 1000
-    x = int(math.sqrt(len(arr)))
-    to_fill = x - (len(arr) % x)
+def generate_models():
+    init()
+    # Define number of existing datasets
+    n_datasets = 1
+    while os.path.exists(dataset_name + '_' + str(n_datasets)):
+        n_datasets += 1
 
-    for i in range(to_fill):
-        arr = np.append(arr, [0])
-    # print(len(arr))
-    n_array = array(arr)
-    matrix = n_array.reshape((x, -1))
-    # matrix = preprocessing.normalize(matrix)
-    matrix = preprocessing.minmax_scale(matrix)
-    plt.imshow(matrix, cmap='Spectral')
-    plt.colorbar()
-    plt.show()
+    for n in range(1, n_datasets):
+        dataset = load_dataset(n)
+
+        for song in range(len(dataset)):
+            print(dataset.loc[song, ['Title', 'genre']])
+            genre = dataset.loc[song, 'genre']
+            series = dataset.loc[song, attrs[len(attrs) - 2]]
+
+            # Todo order in dataset creation
+            series.sort(axis=0)
+
+            mfcc = AudioFeatures().get_perform_mfcc(series, sr)
+            model = genres_dfs[genre]['model']
+            for i in range(rows):
+                for j in range(columns):
+                    model.iloc[i, j] += mfcc[i, j]
+            genres_dfs[genre]['n_songs'] += 1
+
+    for genre in genres_dfs:
+        model = genres_dfs[genre]['model']
+        n_songs = genres_dfs[genre]['n_songs']
+        for i in range(rows):
+            for j in range(columns):
+                model.iloc[i, j] = model.iloc[i, j] / n_songs
+        AudioFeatures().plot_perform_mfcc_by_values(model, sr)
+        model.to_pickle('ImageModel_' + genre)
 
 
-dataset = load_dataset()
-# song = 10
-# print(dataset.loc[song, ['Title', 'genre']])
-# array_to_img(dataset.loc[song, attrs[len(attrs) - 2]])
-
-for song in range(len(dataset)):
-    print(dataset.loc[song, ['Title', 'genre']])
-    array_to_img(dataset.loc[song, attrs[len(attrs) - 2]])
-
-# spectral_analisys()
-# clustering()
+# load_image_model('ImageModel_Blues')
+generate_models()
