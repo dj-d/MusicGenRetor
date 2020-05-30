dae4442fbb93d8b68b8b2aa3e1c653d1692bafff
zappaboy <federico_zappone@hotmail.it>
2020-05-30 03:42:17 +0200

    Added comparing between song and models

--- src/logic.py
+++ src/logic.py
@@ -2,6 +2,7 @@ import os
 
 import numpy as np
 import pandas as pd
+from sklearn.metrics import mean_squared_error
 
 from src.audio_features import AudioFeatures
 
@@ -11,6 +12,8 @@ from src.audio_features import AudioFeatures
 attrs = ['Title', 'bpm', 'zero_crossing_rate', 'audio_time_series', 'genre']
 dataset_name = 'Dataset'
 genres = ['Blues', 'Electronic', 'Classical', 'Pop', 'Rock', 'Jazz']
+models_path = 'Models' + '/'
+datasets_path = 'Datasets' + '/'
 genres_dfs = {}
 df = pd.DataFrame(columns=attrs)
 rows = 20
@@ -21,7 +24,7 @@ sr = 22100
 def load_dataset(n):
     global df
     # TODO: create bulk load function
-    return pd.read_pickle(dataset_name + '_' + str(n))
+    return pd.read_pickle(datasets_path + dataset_name + '_' + str(n))
 
 
 def load_image_model(model_name):
@@ -33,9 +36,45 @@ def init():
     # Init dictionary
     for genre in genres:
         genres_dfs[genre] = {
-            'model': pd.DataFrame(np.zeros((rows, columns))),
+            'models': pd.DataFrame(np.zeros((rows, columns))),
             'n_songs': 0
         }
+
+
+def plot_models():
+    for genre in genres:
+        model_file = models_path + 'ImageModel_' + genre
+        if os.path.exists(model_file):
+            load_image_model(model_file)
+        else:
+            print('No models file for ' + genre)
+
+
+def image_compare(image_a, image_b):
+    return mean_squared_error(image_a, image_b)
+
+
+def compare_song(song_path):
+    audio_features = AudioFeatures(song_path)
+    series = audio_features.get_audio_time_series()
+
+    # Todo order in dataset creation
+    series.sort(axis=0)
+
+    mfcc = audio_features.get_perform_mfcc(series, sr)
+    song = pd.DataFrame(np.zeros((rows, columns)))
+    for i in range(rows):
+        for j in range(columns):
+            song.iloc[i, j] += mfcc[i, j]
+    # song_image = audio_features.plot_perform_mfcc_by_values(models, sr)
+
+    result = {}
+    for genre in genres:
+        model = pd.read_pickle(models_path + 'ImageModel_' + genre)
+        compare_value = image_compare(song, model)
+        result[genre] = compare_value
+
+    return result
 
 
 def generate_models():
@@ -57,22 +96,24 @@ def generate_models():
             series.sort(axis=0)
 
             mfcc = AudioFeatures().get_perform_mfcc(series, sr)
-            model = genres_dfs[genre]['model']
+            model = genres_dfs[genre]['models']
             for i in range(rows):
                 for j in range(columns):
                     model.iloc[i, j] += mfcc[i, j]
             genres_dfs[genre]['n_songs'] += 1
 
     for genre in genres_dfs:
-        model = genres_dfs[genre]['model']
+        model = genres_dfs[genre]['models']
         n_songs = genres_dfs[genre]['n_songs']
         for i in range(rows):
             for j in range(columns):
                 model.iloc[i, j] = model.iloc[i, j] / n_songs
         if model.notnull().all().any():
             AudioFeatures().plot_perform_mfcc_by_values(model, sr)
-            model.to_pickle('ImageModel_' + genre)
+            model.to_pickle(models_path + 'ImageModel_' + genre)
 
 
-# load_image_model('ImageModel_Jazz')
 generate_models()
+plot_models()
+
+# print(compare_song())
