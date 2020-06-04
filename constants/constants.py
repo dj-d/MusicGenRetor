84abbb643d1f74a997ce186d8dd1766133636548
Daniele Albanese <daniele.albanese98@gmail.com>
2020-06-04 12:54:35 +0200

    Added constants.py file with all global constants

---
+++ constants/constants.py
@@ -0,0 +1,18 @@
+SONGS_PATH = ""
+
+DATASET_DIRECTORY = "Datasets" + "/"
+
+TRAINING_DATASET_PATH = DATASET_DIRECTORY + "Training" + "/"
+TESTING_DATASET_PATH = DATASET_DIRECTORY + "Testing" + "/"
+
+MODELS_PATH = "Models" + "/"
+
+DATASET_NAME = "Dataset"
+
+ATTRS = ['Title', 'bpm', 'zero_crossing_rate', 'audio_time_series', 'genre']
+GENRES = ['Blues', 'Electronic', 'Classical', 'Pop', 'Rock', 'Jazz']
+
+SR = 22100
+
+ROW = 20
+COLUMNS = 49
